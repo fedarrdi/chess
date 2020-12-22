5a5202079b80df3901ded138a885cfe3a5176a98
unknown <fedarradi@gmail.com>
2020-12-22 11:20:30 +0200

    git commit master

--- ai.c
+++ ai.c
@@ -17,7 +17,24 @@ enum bool enum_board(enum bool start, struct move *move) // enumerates all squar
     return  false;
 }
 
-int find_best_move(struct move *move, int *out_eval, enum color player, int depth)
+ int find_best_move(struct move *move, int *out_eval, enum color player, int depth)
 {
+    struct move curr_move = {0, 0};
+    struct undo taken;
+
+    if(!depth) return 0;
+
+    while(enum_board(0, &curr_move))
+    {
+        if(board[move->from.y][move->from.x].color == player)
+        {
+
+            piece[board[move->from.y][move->from.x].type].play_move(curr_move, &taken);
+
+            find_best_move(move, out_eval, !player, depth - 1);
+
+            undo_move(curr_move, taken);
+        }
+    }
     return 0;
 }
