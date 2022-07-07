fba74262da828b1000a57a9ea6eacf8a86c978ae
Radoslav Filipov <42972410+fedarrdi@users.noreply.github.com>
2022-07-07 15:05:54 +0300

    Add files via upload

---
+++ chess_types.h
@@ -0,0 +1,39 @@
+#ifndef CHESS_BIT_BOARDS_CHESS_TYPES_H
+#define CHESS_BIT_BOARDS_CHESS_TYPES_H
+
+#define GET_BIT(bitboard, square) (bitboard & (1ULL << square))
+#define SET_BIT(bitboard, square) (bitboard |= (1ULL << square))
+#define POP_BIT(bitboard, square) ( GET_BIT(bitboard, square) ? (bitboard ^= (1ULL << square)) : 0 )
+
+typedef unsigned long long Bitboard;
+
+enum
+{
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1
+};
+
+enum color {black, white, both};
+enum piece {w_pawn, w_knight, w_bishop, w_rook, w_queen, w_king, b_pawn, b_knight, b_bishop, b_rook, b_queen, b_king};
+enum file {FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H};
+enum rank {RANK_8, RANK_7, RANK_6, RANK_5, RANK_4, RANK_3, RANK_2, RANK_1};
+
+typedef struct bit_chess_board
+{
+    Bitboard occupied[3];///by white by black by both
+    /// index w_pawn, w_knight, w_bishop, w_queen, w_rook, w_king, b_pawn, b_knight, b_bishop, b_queen, b_rook, b_king
+    Bitboard pieces[12];///every piece bitboard
+} ChessBoard;
+
+typedef struct bit_lookup_table
+{
+    Bitboard ClearRank[8], MaskRank[8], ClearFile[8], MaskFile[8];
+}LookupTable;
+
+#endif //CHESS_BIT_BOARDS_CHESS_TYPES_H
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/fedar/CLionProjects/chess
+# Install script for directory: C:/Users/fedar/CLionProject/chess_bit_boards
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/chess")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/chess_bit_boards")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/fedar/CLionProjects/chess/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/fedar/CLionProject/chess_bit_boards/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
